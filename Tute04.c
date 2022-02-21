161b39af49a9e031d333ab9ab31e7e8e5a0d15d1
Chamindu Nimsara <99786073+IT21232886@users.noreply.github.com>
2022-02-21 16:25:40 +0000

    Finished all exercises

--- Tute04.c
+++ Tute04.c
@@ -5,6 +5,49 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
+int minimum(int no2, int no1);
+int maximum(int no1, int no2);
+int multiply(int no1, int no2);
+
+int minimum(int no2, int no1)
+{
+  int min;
+   if(no1<no2)
+   {
+     min= no1;
+   }
+   else if(no2<no1)
+   {
+      min = no2;
+   }
+   else
+   {
+     printf("Number are equal");
+   }
+   return min;
+}
+int maximum(int no1, int no2)
+{
+   int max;
+   if(no1>no2)
+   {
+     max= no1;
+   }
+   else if(no2>no1)
+   {
+     max=no2;
+   }
+   else{
+     printf("NUmber are equal");
+   }
+   return max;
+}
+int multiply(int no1 , int no2)
+{
+  int mul;
+  mul = no1 * no2;
+  return mul;
+}
 
 int main() {
    int no1, no2;
